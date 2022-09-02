55bc40116ec932eed2bf7f0d3bce5ec4ecedab25
parkjaehyeong <windowrains@naver.com>
2022-09-02 12:56:24 +0900

    스타일 컴파일 모듈화 구현

--- main.js
+++ main.js
@@ -1,25 +1,14 @@
+// import
 import objData from "./trainingData.js"
 import head from "./header.js"
 import thum from "./thumnail.js"
+import styleTrans from "./styleCompile.js";
 import styleData from './styleData.json' assert{type: "json"};
-
-// const styleData2 = JSON.parse(styleData)
-// console.log()
-
+// 모아모아
 const root = document.body
-const headStyle = document.createElement("style")
-let style ="";
 const {header, title, colorAssets, thumnailData} = objData
 const imageURL = ["./images/image1.jpg", "./images/image2.jpg", "./images/image3.jpg"]
 root.innerHTML += head(header)
 root.innerHTML += thum(thumnailData, imageURL)
-console.dir(Object.values(Object.keys(styleData))[8])
-console.log(Object.keys(styleData).length)
-for(let i = 0; i < Object.keys(styleData).length; i++){
-console.dir(JSON.stringify(Object.values(styleData)[i]))
+styleTrans(styleData);
 
-  style += `${Object.values(Object.keys(styleData))[i]} ${JSON.stringify(Object.values(styleData)[i]).replace(/\"/g, "").replace(/\,/g , ";")}`
-}
-console.log(style)
-headStyle.innerHTML = style
-document.head.append(headStyle)
